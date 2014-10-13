9c61e099ddc8659ef872ca419b9adaeab3121821
Austin Heiman <atheimanksu@gmail.com>
2014-10-13 16:09:19 -0500

    admin actions added to update status of many games simultaneously

--- pickem_site/pickem_app/admin.py
+++ pickem_site/pickem_app/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from pickem_app.models import *
+from pickem_app.global_vars import *
 
 admin.site.site_header = "PickEm Administration"
 admin.site.site_title = "PickEm Adminstration"
@@ -10,15 +11,16 @@ admin.site.index_title = "PickEm Administration Home"
 
 
 
-# class PickerInline(admin.StackedInline):
-# 	model = Picker
-# 	can_delete = False
-# 	verbose_name_plural = 'picker'
+# def make_published(modeladmin, request, queryset):
+#     queryset.update(status='p')
+# make_published.short_description = "Mark selected stories as published"
 
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'status']
+#     ordering = ['title']
+#     actions = [make_published]
 
-
-# class UserAdmin(admin.ModelAdmin):
-# 	inlines = (PickerInline, )
+# admin.site.register(Article, ArticleAdmin)
 
 
 
@@ -26,6 +28,24 @@ class TeamAdmin(admin.ModelAdmin):
 	fields = ['abbreviation', 'location', 'nickname']
 	list_display = ['id', 'abbreviation', 'location', 'nickname']
 	# search_fields = ['abbreviation', 'location', 'nickname']
+
+
+
+def make_in_progress(modeladmin, request, queryset):
+	queryset.update(status=IN_PROGRESS)
+make_in_progress.short_description = "Mark selected games as In Progress"
+
+
+
+def make_not_yet_started(modeladmin, request, queryset):
+	queryset.update(status=NOT_YET_STARTED)
+make_in_progress.short_description = "Mark selected games as Not Yet Started"
+
+
+
+def make_complete(modeladmin, request, queryset):
+	queryset.update(status=COMPLETE)
+make_in_progress.short_description = "Mark selected games as Complete"
 
 
 
@@ -45,6 +65,7 @@ class GameAdmin(admin.ModelAdmin):
 	list_display = ['id', 'week', 'away_team', 'home_team', 'status', 'date_time', 'spread', 'spread_winner']
 	list_filter = ['week',]
 	ordering = ['date_time']
+	actions = [make_in_progress, make_not_yet_started, make_complete]
 	# search_fields = ['week', 'away_team', 'home_team']
 
 
