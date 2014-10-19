d2941343f6967b208d19416f5aa41916505ab946
Austin Heiman <atheimanksu@gmail.com>
2014-10-19 14:15:14 -0500

    corrected weekStartDates

--- pickem_site/pickem_app/static/pickem_app/functions.js
+++ pickem_site/pickem_app/static/pickem_app/functions.js
@@ -1,9 +1,27 @@
-var weekStartDates = [new Date('Apr 1, 2014'), new Date('Sep 10, 2014')]
-
-for (var i = 2; i <= 21; i++) {
-	weekStartDates[i] = new Date();
-	weekStartDates[i].setDate(weekStartDates[1].getDate()+7*(i-1));
-}
+var weekStartDates = [
+	new Date('Apr 1 2014'),   	// wk 1 - Sun Sep 7
+	new Date('Sep 10 2014'),   	// wk 2 - Sun Sep 14
+	new Date('Sep 17 2014'),   	// wk 3 - Sun Sep 21
+	new Date('Sep 24 2014'),   	// wk 4 - Sun Sep 28
+	new Date('Oct 1 2014'),   	// wk 5 - Sun Oct 5
+	new Date('Oct 8 2014'),   	// wk 6 - Sun Oct 12
+	new Date('Oct 15 2014'),   	// wk 7 - Sun Oct 19
+	new Date('Oct 22 2014'),   	// wk 8 - Sun Oct 26
+	new Date('Oct 29 2014'),   	// wk 9 - Sun Nov 2
+	new Date('Nov 5 2014'),   	// wk 10 - Sun Nov 9
+	new Date('Nov 12 2014'),   	// wk 11 - Sun Nov 16
+	new Date('Nov 19 2014'),   	// wk 12 - Sun Nov 23
+	new Date('Nov 26 2014'),   	// wk 13 - Sun Nov 30
+	new Date('Dec 3 2014'),   	// wk 14 - Sun Dec 7
+	new Date('Dec 10 2014'),   	// wk 15 - Sun Dec 14
+	new Date('Dec 17 2014'),   	// wk 16 - Sun Dec 21
+	new Date('Dec 24 2014'),   	// wk 17 - Sun Dec 28
+	new Date('Dec 31 2014'),   	// Wild Card - Sat, Sun Jan 3, 4
+	new Date('Jan 7 2014'),   	// Divisional - Sat, Sun Jan 10, 11
+	new Date('Jan 14 2014'),   	// Championship - Sun Jan 18
+	                        	// Pro Bowl - Sun Jan 25
+	new Date('Jan 21 2014')   	// Super Bowl - Sun Feb 1
+]
 
 function getWeekStartDate(weekNumber, weekStartDatesArray) {
 	// getWeekStartDate(4) returns the wed before week 4.
